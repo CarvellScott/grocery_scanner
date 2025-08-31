57d4d0a8fc264f2c88a0f8e2cd51ae26e5a5435f
Carvell Scott <carvell.scott@gmail.com>
2025-08-31 18:47:19 -0500

    Added a new dataclass for real-world containers

--- grocery_scanner/models.py
+++ grocery_scanner/models.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import dataclasses
+from datetime import datetime
 
 @dataclasses.dataclass(frozen=True)
-class _GroceryItem:
+class GroceryItem:
     reference: str
     name: str
     url: str
 
-class GroceryItem(_GroceryItem):
-    pass
+@dataclasses.dataclass
+class ItemContainer:
+    reference: str = dataclasses.field(hash=True)
+    content: GroceryItem = dataclasses.field(hash=True)
+    last_request: datetime = dataclasses.field(compare=False, default=None)
+    last_fill: datetime = dataclasses.field(compare=False, default=None)
