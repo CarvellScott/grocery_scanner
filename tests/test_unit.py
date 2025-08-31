2486890f8e089602f580af3406d2aceea3bc9201
Carvell Scott <carvell.scott@gmail.com>
2025-08-31 18:48:50 -0500

    Added a just slightly-less rudimentary test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,14 +2,17 @@
 import unittest
 
 from grocery_scanner import core
+from grocery_scanner import models
+
 
 class TestCoreImport(unittest.TestCase):
     def test_core(self):
         self.assertIsNotNone(core)
+        pass
 
-def main():
-    pass
 
-if __name__ == "__main__":
-    main()
-
+class TestModelImport(unittest.TestCase):
+    def test_model(self):
+        apples = models.GroceryItem("apples", "Apples", "about:blank")
+        apple_box = models.ItemContainer("apple_box", apples)
+        self.assertTrue(apple_box.content == apples)
